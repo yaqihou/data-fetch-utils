b2a40d9110d484bacfaad7fdf5cdfd9fd8174f57
Yaqi Hou <yaqi.hou@yahoo.com>
2024-03-11 20:51:10 -0400

    Update usage help information for wallhaven fetcher
    Signed-off-by: Yaqi Hou <yaqi.hou@yahoo.com>

--- run_wallhaven_fetcher.py
+++ run_wallhaven_fetcher.py
@@ -6,18 +6,22 @@ from src.wallhaven.enums import Purity, Category
 from src.wallhaven.fetcher import DailyFetcher
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--download-files', action='store_true')
-parser.add_argument('-f', '--fetch-details', action='store_true')
-parser.add_argument('-D', '--dir', nargs='?', type=str, default=None)
+parser.add_argument('-d', '--download-files', action='store_true',
+                    help="Whether to download image files (Default False)")
+parser.add_argument('-f', '--fetch-details', action='store_true',
+                    help="Whether to fetch details info, e.g. includ. tags (Default False)")
+parser.add_argument('-D', '--dir', nargs='?', type=str, default=None,
+                    help="Base directory to save wallpaper files, if not given, environment variable WALLHAVEN_DIR or current folder will be used")
 parser.add_argument('-p', '--page', nargs='?', type=str, default='1-50',
                     help="Range of page to dwonload, use dash (-) for range, comma for separation")
-parser.add_argument('-i', '--interval', nargs='?', type=int, default=2)
+parser.add_argument('-i', '--interval', nargs='?', type=int, default=2,
+                    help='Waiting interval between two downloads or requests')
 parser.add_argument('-C', '--categories', nargs='?', type=str, default='111',
-                    help='Flag for categories (General/Anime/People) in form of 101 (Default 111)'
-                    )
+                    help='Flag for categories (General/Anime/People) in form of 101 (Default 111)')
 parser.add_argument('-P', '--purities', nargs='?', type=str, default='110',
                     help='Flag for categories (SFW/Sketchy/NSFW) in form of 110 (Default 110)')
-parser.add_argument('-l', '--log-file', nargs='?', type=str, default=None)
+parser.add_argument('-l', '--log-file', nargs='?', type=str, default=None,
+                    help='Log file to store logging information')
 parser.add_argument('-o', '--keep-output', action='store_true',
                     help='Leave output when set log-file')
 
