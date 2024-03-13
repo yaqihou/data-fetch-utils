a73d0ff86c618263fe19f42682cb2a84da573a1b
Yaqi Hou <yaqi.hou@yahoo.com>
2024-03-12 23:10:12 -0400

    Fix a bug in fetching wallpaper from cache
    Signed-off-by: Yaqi Hou <yaqi.hou@yahoo.com>

--- src/wallhaven/cacher.py
+++ src/wallhaven/cacher.py
@@ -2,6 +2,7 @@
 import os
 import time
 import pickle
+from typing import Optional
 
 from .defs import Wallpaper 
 from ..logger import MyLogger
@@ -23,6 +24,7 @@ class Cache:
 
     def __init__(self):
         logger.debug(f'Loaded {len(self._cache)} entries from cache file {self._cache_file}')
+        self._iter_keys = iter(self._cache.keys())
 
     def __len__(self):
         return len(self._cache)
@@ -30,15 +32,27 @@ class Cache:
     def __contains__(self, val: str):
         return val in self._cache
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self._iter_keys)
+
     def __getitem__(self, key: str):
         return self._cache[key]
+
+    def items(self):
+        return self._cache.items()
 
     def add(self, wallpaper: Wallpaper):
         self._cache[wallpaper.id] = wallpaper.json
 
-    def fetch_wallpaper(self, wid) -> Wallpaper:
-        # let it crash
-        return Wallpaper(self._cache[wid])
+    def fetch_wallpaper(self, wid: str) -> Optional[Wallpaper]:
+        json = self._cache.get(wid, None)
+        if json:
+            return Wallpaper(json)
+        else:
+            return None
 
     def save(self):
         dirname = os.path.dirname(self._cache_file)
